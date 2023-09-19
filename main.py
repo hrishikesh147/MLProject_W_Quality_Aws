d281e238472dabc490c81e4095cb3a424bc39248
hrishikesh147 <hrishikeshbhagawati@gmail.com>
2023-09-19 21:16:27 +0530

    data validation stage complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.mlproject import logger
 from src.mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.mlproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME="Data Ingestion"
@@ -12,3 +13,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e 
+
+
+STAGE_NAME="Data Validation" 
+
+try:
+    logger.info(f"Stage {STAGE_NAME} STARTED...")
+    obj=DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f"Stage {STAGE_NAME} COMPLETED...")
+
+except Exception as e:
+    raise e
